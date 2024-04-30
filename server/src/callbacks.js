14aece38022483f95582be7fa0fb2f4891d0cef3
Lev Rosenberg <lev.rosenberg@gmail.com>
2024-04-30 12:25:02 -0500

    AI wizard functionality works

--- server/src/callbacks.js
+++ server/src/callbacks.js
@@ -2,13 +2,24 @@ import { ClassicListenersCollector } from '@empirica/core/admin/classic'
 export const Empirica = new ClassicListenersCollector()
 
 Empirica.onGameStart(({ game }) => {
-  // Get the treatment condition from the game object.
+  /*
+    1. Fill round with 7 stages --> Instructions, Walkthrough, Role Exploration, Indivudual, Team, MTS, AI Variation.
+      a. AI intervention placement depends on the interventionPlacement treatment condition.
+      b. TODO: add instructions and/or walkthrough stage
+    2. Assign each player a role, and a team.
+      a. If the aiVariation treatment condition is wizard, we want to assign the AI players the roles of AI Specialist and AI Scientist.
+    3. Set their location rankings for the individual team, and mts stages.
+      a. individual rankings held in player.get('individual-ranking')
+      b. team rankings held in round.get('<team-acronym>-team-ranking')
+      c. mts rankings held in round.get('mts-ranking')
+  */
   const treatment = game.get('treatment')
-  const { interventionPlacement } = treatment
+  const { interventionPlacement, aiVariation } = treatment
 
-  // We only want one round, filled with 6 stages. AI intervention placement depends on the treatment condition.
+  // 1: Create the round and stages
   const round = game.addRound({ name: 'Round' })
-  // round.addStage({ name: 'walkthrough', duration: 120 })
+  round.addStage({ name: 'Walkthrough', duration: 120 })
+  round.addStage({ name: 'Instructions', duration: 6000 })
   round.addStage({ name: 'Role Exploration', duration: 6000 })
   round.addStage({ name: 'Individual Ranking', duration: 600 })
   if (interventionPlacement === 'individual') round.addStage({ name: 'intervention', placement: 'Individual Ranking', duration: 600 })
@@ -17,26 +28,31 @@ Empirica.onGameStart(({ game }) => {
   round.addStage({ name: 'Multi-Team Ranking', duration: 600 })
   if (interventionPlacement === 'mts') round.addStage({ name: 'intervention', placement: 'Multi-Team Ranking', duration: 600 })
 
-  // Assign each player a role, and a team.
-  // And set their rankings for the individual and team stages.
-  // By altenating specialist and scientist roles in the list, we ensure that the teams are balanced.
+  // 2: Assign roles and teams
   const possibleRoles = ['Water Specialist', 'Geology Scientist', 'Atmospheric Specialist', 'Climate Scientist', 'Terrain Specialist', 'Life Scientist']
-  // before this loop, get the player/s that are fake AI from partipant ID. player.get("participantIdentifier")). and assign them to like AI Scientist or AI Specialist
+  const aiRoles = ['AI Specialist', 'AI Scientist'] // only used if aiVariation === 'wizard'
   const players = game.players
-  for (let i = 0; i < players.length; i++) {
-    i = i % possibleRoles.length // loop back through the begining
-    const role = possibleRoles[i]
-    players[i].set('role', role)
-    if (role.split(' ')[1] === 'Specialist') {
-      players[i].set('team', 'Planetary Geology Team')
+  for (const player of players) {
+    let role
+    if (aiVariation === 'wizard' && player.get('participantIdentifier').includes('AI')) {
+      role = aiRoles.pop()
+      player.set('role', role)
     } else {
-      players[i].set('team', 'Space Human Factors')
+      const i = (players.indexOf(player) - (2 - aiRoles.length)) % possibleRoles.length // loop back through the beginning
+      role = possibleRoles[i]
+      player.set('role', role)
     }
-    players[i].set('individual-ranking', ['Argyre', 'Casius', 'Diacria', 'Eridania'])
-    players[i].set('team-ranking', ['Argyre', 'Casius', 'Diacria', 'Eridania'])
+    if (role.split(' ')[1] === 'Specialist') {
+      player.set('team', 'Planetary Geology Team')
+    } else {
+      player.set('team', 'Space Human Factors')
+    }
+    // 3a: Set individual rankings
+    player.set('individual-ranking', ['Argyre', 'Casius', 'Diacria', 'Eridania'])
   }
-  // We set the mts ranking outside of the loop, because it is the same for all players.
-  // stage.set('Space Human Factors', ['Argyre', 'Casius', 'Diacria', 'Eridania'])
+  // 3b, 3c: Set team, mts rankings
+  round.set('shf-team-ranking', ['Argyre', 'Casius', 'Diacria', 'Eridania'])
+  round.set('pgt-team-ranking', ['Argyre', 'Casius', 'Diacria', 'Eridania'])
   round.set('mts-ranking', ['Argyre', 'Casius', 'Diacria', 'Eridania'])
 })
 
