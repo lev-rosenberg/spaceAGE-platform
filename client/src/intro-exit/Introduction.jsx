450913090fcbbad358c558f0d25821f0843c7272
Angelina <angelinachin2026@u.northwestern.edu>
2024-10-28 20:45:08 -0500

    embedded video and next button disabled

--- client/src/intro-exit/Introduction.jsx
+++ client/src/intro-exit/Introduction.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '../components/Button';
 
 export function Introduction({ next }) {
+  const [isButtonDisabled, setButtonDisabled] = useState(true); // initially disable button
+
+  useEffect(() => {
+    // enable button after 1 min
+    const timer = setTimeout(() => {
+      setButtonDisabled(false);
+    }, 60000); 
+    return () => clearTimeout(timer); // clean up timer on component unmount
+  }, []);
+
   return (
     <div className="mt-3 sm:mt-5 p-20">
       <h3 className="text-lg leading-6 font-medium">
@@ -16,14 +26,13 @@ export function Introduction({ next }) {
         <iframe
           width="853"
           height="480"
-          src="https://www.youtube.com/embed/2u72Nwd0zkU" // Directly embed the video
-          frameBorder="0"
+          src="https://www.youtube.com/embed/Q3_7b-S5L_4" // Directly embed the video
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
           title="Embedded youtube"
         />
       </div>
-      <Button handleClick={next} autoFocus>
+      <Button handleClick={next} autoFocus disabled={isButtonDisabled}>
         Next
       </Button>
     </div>
